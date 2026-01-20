b499c4c170a90fa4c8404ba7d82afe3f92e33857
Nausheen Faiyaz <logme2nausheen@gmail.com>
2026-01-20 15:18:25 +0530

    Remove unused slider functionality from script.js

--- script.js
+++ script.js
@@ -130,57 +130,6 @@ document.addEventListener("DOMContentLoaded", function () {
   animateSVGPaths(); // Initial check in case some SVGs are already in view
 });
 
-// Slider
-const slider = function () {
-  const slides = document.querySelectorAll(".slide");
-  const btnLeft = document.querySelector(".slider__btn--left");
-  const btnRight = document.querySelector(".slider__btn--right");
-
-  let curSlide = 0;
-  const maxSlide = slides.length;
-
-  const goToSlide = function (slide) {
-    slides.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-    );
-  };
-
-  // Next slide
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-
-    goToSlide(curSlide);
-  };
-
-  const prevSlide = function () {
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-    goToSlide(curSlide);
-  };
-
-  const init = function () {
-    goToSlide(0);
-  };
-  init();
-
-  // Event handlers
-  btnRight.addEventListener("click", nextSlide);
-  btnLeft.addEventListener("click", prevSlide);
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") prevSlide();
-    e.key === "ArrowRight" && nextSlide();
-  });
-};
-slider();
-
 window.addEventListener("scroll", () => {
   const scrollTop = window.scrollY;
   const windowHeight = window.innerHeight;
